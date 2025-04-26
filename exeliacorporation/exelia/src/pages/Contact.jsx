3ab77534a857186ad0b4410c049c0b6dd85c02e6
Gabriela <gabrielamalasquez126@gmail.com>
2025-04-26 14:35:47 +0200

    numero contacto

--- exeliacorporation/exelia/src/pages/Contact.jsx
+++ exeliacorporation/exelia/src/pages/Contact.jsx
@@ -12,19 +12,17 @@ export default function Contact() {
         {/* Tarjeta para Mario */}
         <div className="contact-card">
           <h3>Mario</h3>
-          <p><strong>Teléfono:</strong> +34 666 666 666</p>
-          <p><strong>Email:</strong> <a href="mailto:mario@exelia.com">mario@exelia.com</a></p>
+          <p><strong>Teléfono:</strong> +34 610 197 239</p>
+          <p><strong>Email:</strong> <a href="mailto:exeliastore@gmail.com">exeliastore@gmail.com</a></p>
         </div>
 
         {/* Tarjeta para Gabriela */}
         <div className="contact-card">
           <h3>Gabriela</h3>
-          <p><strong>Teléfono:</strong> +34 222 222 222</p>
-          <p><strong>Email:</strong> <a href="mailto:gabriela@exelia.com">gabriela@exelia.com</a></p>
+          <p><strong>Teléfono:</strong> +34 692 2092 04</p>
+          <p><strong>Email:</strong> <a href="mailto:exeliastore@gmail.com">exeliastore@gmail.com</a></p>
         </div>
       </div>
-
-     
     </div>
   );
 }
